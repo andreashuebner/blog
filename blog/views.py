4badb218f08453e634c64122ea040f4935aa4c17
andreashuebner <andreas.huebnerh@gmail.com>
2015-04-05 18:50:19 +0000

    Adding functions to add, delete and edit posts.

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,88 @@ from flask import render_template
 from blog import app
 from .database import session
 from .models import Post
+import mistune
+from flask import request, redirect, url_for
 
 @app.route("/")
-def posts():
+@app.route("/page/<int:page>")
+def posts(page=1, paginate_by=10):
+    # Zero-indexed page
+    page_index = page - 1
+
+    count = session.query(Post).count()
+
+    start = page_index * paginate_by
+    end = start + paginate_by
+
+    total_pages = (count - 1) / paginate_by + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
     posts = session.query(Post)
     posts = posts.order_by(Post.datetime.desc())
-    posts = posts.all()
+    posts = posts[start:end]
+
     return render_template("posts.html",
-        posts=posts
+        posts=posts,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
     )
+
+@app.route("/post/<int:id>/edit",methods=["POST"])
+def post_edit(id):
+    post = session.query(Post).get(id)
+    post.title=request.form["title"],
+    post.content=mistune.markdown(request.form["content"])
+    session.commit()
+    return redirect(url_for("posts"))
+    
+@app.route("/post/<int:id>/delete",methods=["GET"])
+def get_delete(id):
+    post = session.query(Post).get(id)
+    referrer = request.referrer
+    return render_template("post_delete.html",
+                           post = post,
+                           referrer = referrer
+                           )
+@app.route("/post/<int:id>/edit", methods=["GET"])
+def get_edit(id):
+    post = session.query(Post).get(id)
+    referrer = request.referrer #in case user wants to go back instead of editing
+    return render_template("post_edit.html",
+                           post = post,
+                           referrer = referrer
+                           )
+    
+@app.route("/post/<int:id>")
+def post_detail(id):
+    post = session.query(Post).get(id)
+    #get where user came from
+    referrer = request.referrer
+    return render_template("post_detail.html",
+                           post=post,
+                           referrer=referrer
+                           )
+@app.route("/post/add", methods=["GET"])
+def add_post_get():
+    
+    return render_template("add_post.html")
+    
+    
+@app.route("/post/<int:id>/delete",methods=["POST"])
+def delete_post(id):
+    post = session.query(Post).get(id)
+    session.delete(post)
+    session.commit()
+    return redirect(url_for("posts"))
+@app.route("/post/add", methods=["POST"])
+def add_post_post():
+    post = Post(
+        title=request.form["title"],
+        content=mistune.markdown(request.form["content"]),
+    )
+    session.add(post)
+    session.commit()
+    return redirect(url_for("posts"))
